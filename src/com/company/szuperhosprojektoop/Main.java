f2cc63c0fd407324319a2dfff8fca7298389cfb1
Takács Balázs <tbalazs.levente@gmail.com>
2021-10-21 11:14:41 +0200

    Superhos interfész létrehozás

--- src/com/company/szuperhosprojektoop/Main.java
+++ src/com/company/szuperhosprojektoop/Main.java
@@ -3,6 +3,6 @@ package com.company.szuperhosprojektoop;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
---
+++ src/com/company/szuperhosprojektoop/Szuperhos.java
@@ -0,0 +1,6 @@
+package com.company.szuperhosprojektoop;
+
+public interface Szuperhos {
+    boolean legyoziE(Szuperhos sz);
+    Szuperhos egymekkoraAzEreje();
+}
